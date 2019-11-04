a95ce3d79bd4fa41207efbdafdcf46620e6c6c0f
Jonathan Arbely <dev@jonathanarbely.de>
2019-11-05 01:02:58 +0200

    build 3a8ee4b

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 "use strict";
 console.log("WORKER: executing.");
-var version = "v16::",
+var version = "v17::",
     offlineFundamentals = ['',
                             'assets/stylesheets/artofmagellan.css',
                             'assets/stylesheets/pagespecific/index.css',
